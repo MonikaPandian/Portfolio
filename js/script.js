e0f146f46b2715b0bf650ded95f1b0c84808aa67
MonikaPandian <panmonikmm@gmail.com>
2023-02-06 18:59:47 +0530

    portfolio

--- js/script.js
+++ js/script.js
@@ -53,21 +53,21 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-(function () {
-    emailjs.init('GK9hUn4WxlbieGz0h');
-})();
+// (function () {
+//     emailjs.init('GK9hUn4WxlbieGz0h');
+// })();
 
-window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function () {
-                console.log('SUCCESS!');
-            }, function (error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
+// window.onload = function () {
+//     document.getElementById('contact-form').addEventListener('submit', function (event) {
+//         event.preventDefault();
+//         // generate a five digit number for the contact_number variable
+//         this.contact_number.value = Math.random() * 100000 | 0;
+//         // these IDs from the previous steps
+//         emailjs.sendForm('contact_service', 'contact_form', this)
+//             .then(function () {
+//                 console.log('SUCCESS!');
+//             }, function (error) {
+//                 console.log('FAILED...', error);
+//             });
+//     });
+// }
